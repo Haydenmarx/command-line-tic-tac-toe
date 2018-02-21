4749596f14d58bc79eb00c5c4a34e18641791398
Hayden Marx <haydenmarx@gmail.com>
2018-02-20 17:35:52 -0800

    Added validation

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,34 +1,5 @@
-// early board design
-// console.log("|-----------|");
-// console.log("| 1 | 2 | 3 |");
-// console.log("|-----------|");
-// console.log("| 4 | 5 | 6 |");
-// console.log("|-----------|");
-// console.log("| 7 | 8 | 9 |");
-// console.log("|-----------|");
-
-// console.log("|-----------|");
-// console.log("| X | O | X |");
-// console.log("|-----------|");
-// console.log("| X | O | O |");
-// console.log("|-----------|");
-// console.log("| O | X | X |");
-// console.log("|-----------|");
-
-//new board design
-// console.log(" 1 | 2 | 3 ");
-// console.log("-----------");
-// console.log(" 4 | 5 | 6 ");
-// console.log("-----------");
-// console.log(" 7 | 8 | 9 ");
-
-// console.log(" O | X | O ");
-// console.log("-----------");
-// console.log(" X | X | O ");
-// console.log("-----------");
-// console.log(" X | O | X ");
-
 var readline = require('readline');
+
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -37,69 +8,59 @@ var rl = readline.createInterface({
 
 class TicTacToe {
   constructor() {
-    this.board = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-    ];
+    this.board = {
+      1: 1,
+      2: 2,
+      3: 3,
+      4: 4,
+      5: 5,
+      6: 6,
+      7: 7,
+      8: 8,
+      9: 9,
+    }
     this.currentPiece = 'X';
     this.movesLeft = 9;
   }
   displayBoard() {
     console.log(
-      ' ' + this.board[0][0] + ' | ' + this.board[0][1] + ' | ' + this.board[0][2] +
+      ' ' + this.board[1] + ' | ' + this.board[2] + ' | ' + this.board[3] +
       ' \n----------\n' +
-      ' ' + this.board[1][0] + ' | ' + this.board[1][1] + ' | ' + this.board[1][2] +
+      ' ' + this.board[4] + ' | ' + this.board[5] + ' | ' + this.board[6] +
       ' \n----------\n' +
-      ' ' + this.board[2][0] + ' | ' + this.board[2][1] + ' | ' + this.board[2][2]
+      ' ' + this.board[7] + ' | ' + this.board[8] + ' | ' + this.board[9]
      );
+  }
+  
+  checkWinningMoves(num) {
+    switch(num) {
+      case '1':
+        console.log(num);
+        break;
+      default:
+        console.log(false);
+    }
   }
 
 }
-
-
-// rl.question('Do you want to play Tic Tac Toe? [Yes or No] ', (answer) => {
-//   if (answer.toLowerCase() === 'yes' ) {
-//     var game = new TicTacToe();
-//     if (game.movesLeft > 0) {
-//       game.displayBoard();
-//       num = rl.question('Pick a number? ');
-//       console.log(num);
-//       // rl.question('Pick a number?', move => {
-//       //   game.movesLeft--;
-//       //   if (num < 4) {
-//       //     game.board[0][move] = 'X';
-//       //   } else if (num < 7) {
-//       //     game.board[0][move-3] = 'X';
-//       //   } else if (num < 10) {
-//       //     game.board[0][move-6] = 'X';          
-//       //   }
-//       // rl.close();
-//       // });
-//     }
-
-//   }
-//   rl.close();
-// });
 
 var game = new TicTacToe();
 game.displayBoard();
 console.log(game.currentPiece, " Pick a Number: ")
 rl.on('line', function(move) {
-
-  if (move < 4) {
-    game.board[0][move-1] = game.currentPiece;
-  } else if (move < 7) {
-    game.board[1][move-4] = game.currentPiece;
-  } else if (move < 10) {
-    game.board[2][move-7] = game.currentPiece;          
-  }
-  game.movesLeft--;
-  game.displayBoard();
-  if (game.movesLeft === 0) {
-    rl.close();
-  }
-  game.currentPiece === 'X' ? game.currentPiece = 'O' : game.currentPiece = 'X';
-  console.log(game.currentPiece, " Pick a Number: ")
+  if (game.board[move] !== Number(move)) {
+    game.displayBoard();
+    console.log("INVALID MOVE ", game.currentPiece);
+  } else {
+    game.checkWinningMoves(move);
+    game.movesLeft--;
+    game.board[move] = game.currentPiece;
+    if (game.movesLeft === 0) {
+      rl.close();
+    }
+    game.displayBoard();
+    game.currentPiece === 'X' ? game.currentPiece = 'O' : game.currentPiece = 'X';
+    console.log(game.currentPiece, " Pick a Number: ")
+  }  
 })
 

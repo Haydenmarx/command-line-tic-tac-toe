227afc01551b7884403bc5e45985f18aa7ac3aa3
Hayden Marx <haydenmarx@gmail.com>
2018-02-20 16:58:49 -0800

    Paritally working, theory down

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -28,20 +28,71 @@
 // console.log("-----------");
 // console.log(" X | O | X ");
 
-const board = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-]
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
 
-displayBoard = () => {
-  console.log(
-   ' ' + board[0][0] + ' | ' + board[0][1] + ' | ' + board[0][2] +
-   ' \n----------\n' +
-   ' ' + board[1][0] + ' | ' + board[1][1] + ' | ' + board[1][2] +
-   ' \n----------\n' +
-   ' ' + board[2][0] + ' | ' + board[2][1] + ' | ' + board[2][2]
-  );
+class TicTacToe {
+  constructor() {
+    this.board = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ]
+    this.movesLeft = 9;
+  }
+  displayBoard() {
+    console.log(
+      ' ' + this.board[0][0] + ' | ' + this.board[0][1] + ' | ' + this.board[0][2] +
+      ' \n----------\n' +
+      ' ' + this.board[1][0] + ' | ' + this.board[1][1] + ' | ' + this.board[1][2] +
+      ' \n----------\n' +
+      ' ' + this.board[2][0] + ' | ' + this.board[2][1] + ' | ' + this.board[2][2]
+     );
+  }
+
 }
 
-displayBoard();
+
+// rl.question('Do you want to play Tic Tac Toe? [Yes or No] ', (answer) => {
+//   if (answer.toLowerCase() === 'yes' ) {
+//     var game = new TicTacToe();
+//     if (game.movesLeft > 0) {
+//       game.displayBoard();
+//       num = rl.question('Pick a number? ');
+//       console.log(num);
+//       // rl.question('Pick a number?', move => {
+//       //   game.movesLeft--;
+//       //   if (num < 4) {
+//       //     game.board[0][move] = 'X';
+//       //   } else if (num < 7) {
+//       //     game.board[0][move-3] = 'X';
+//       //   } else if (num < 10) {
+//       //     game.board[0][move-6] = 'X';          
+//       //   }
+//       // rl.close();
+//       // });
+//     }
+
+//   }
+//   rl.close();
+// });
+
+var game = new TicTacToe();
+game.displayBoard();
+console.log("Pick a Number: ")
+rl.on('line', function(move) {
+  if (move < 4) {
+    game.board[0][move-1] = 'X';
+  } else if (move < 7) {
+    game.board[1][move-4] = 'X';
+  } else if (move < 10) {
+    game.board[2][move-7] = 'X';          
+  }
+  game.displayBoard();
+  console.log("Pick a Number: ")
+
+})
